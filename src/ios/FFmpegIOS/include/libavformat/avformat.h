c8a453f92b8936feee4544f9519393bfefc700e5
sezer.korkmaz@armakom.com <sezer.korkmaz@armakom.com>
2017-11-09 16:23:30 +0300

    Update FFMpeg Library

--- src/ios/FFmpegIOS/include/libavformat/avformat.h
+++ src/ios/FFmpegIOS/include/libavformat/avformat.h
@@ -2440,7 +2440,7 @@ void avformat_close_input(AVFormatContext **s);
  *
  * @see av_opt_find, av_dict_set, avio_open, av_oformat_next, avformat_init_output.
  */
-av_warn_unused_result
+
 int avformat_write_header(AVFormatContext *s, AVDictionary **options);
 
 /**
@@ -2462,7 +2462,7 @@ int avformat_write_header(AVFormatContext *s, AVDictionary **options);
  *
  * @see av_opt_find, av_dict_set, avio_open, av_oformat_next, avformat_write_header.
  */
-av_warn_unused_result
+
 int avformat_init_output(AVFormatContext *s, AVDictionary **options);
 
 /**
